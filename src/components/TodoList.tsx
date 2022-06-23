e201b8f8e942c018dcc3b9b5092cf1f6300592f9
epthalite <fmy628.1998@icloud.com>
2022-06-23 23:58:32 +0900

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useContext } from 'react'
+import { client } from '../libs/axios'
+import { TodoContext } from '../provider/TodoProvider'
+
+const omitText = (text: string): string => {
+  if (text.length > 10) {
+    return text.substring(0, 7) + '...'
+  }
+  return text
+}
+
+export const TodoList = () => {
+  const { todos, setTodos } = useContext(TodoContext)
+
+  useEffect(() => {
+    client.get('todo/fetch-todos').then(({ data }) => {
+      setTodos(data)
+    })
+  }, [setTodos])
+
+  const changeTodo = async (id: string, status: string) => {
+    await client.post('todo/change-todo', { id, status })
+    client.get('todo/fetch-todos').then(({ data }) => {
+      setTodos(data)
+    })
+  }
+
+  const deleteTodo = async (id: string) => {
+    await client.post('todo/delete-todo', { id })
+    client.get('todo/fetch-todos').then(({ data }) => {
+      setTodos(data)
+    })
+  }
+
+  return (
+    <div className="p-4 border border-gray-200 rounded shadow-lg">
+      <p className="font-bold mb-2">タスク一覧</p>
+      <table className="border-collapse table-auto">
+        <thead>
+          <tr>
+            <th className="py-1">番号</th>
+            <th className="p-1">タスク名</th>
+            <th className="p-1">ステータス</th>
+            <th className="p-1"></th>
+            <th className="p-1"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, index) => {
+            return (
+              <tr key={index}>
+                <td className="p-1">{index + 1}</td>
+                <td className="p-1">{omitText(todo.name)}</td>
+                <td className="p-1">{todo.status}</td>
+                <td className="p-1">
+                  <button
+                    className="px-2 h-7 border border-white rounded bg-teal-400 shadow-md text-white"
+                    onClick={() => {
+                      changeTodo(todo.id, todo.status)
+                    }}
+                  >
+                    変更する
+                  </button>
+                </td>
+                <td className="p-1">
+                  <button
+                    className="px-2 h-7 border border-white rounded bg-teal-400 shadow-md text-white"
+                    onClick={() => {
+                      deleteTodo(todo.id)
+                    }}
+                  >
+                    削除する
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
